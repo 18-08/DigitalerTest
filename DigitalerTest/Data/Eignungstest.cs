ea4ad532b9e7582eff60e90b7aa7a5a19521c4e2
Marcel Reinhardt <post4marcel@live.de>
2020-06-17 13:27:11 +0200

    Startseite + einzelne Änderungen

---
+++ DigitalerTest/Data/Eignungstest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalerTest.Data
+{
+    public class Eignungstest
+    {
+        public List<Themengebiete> themengebiete = new List<Themengebiete>();
+    }
+}
---
+++ DigitalerTest/Data/Themengebiete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalerTest.Data
+{
+    public class Themengebiete
+    {
+        public string titel { get; set; }
+        public List<Frage> fragen = new List<Frage>();
+    }
+}
