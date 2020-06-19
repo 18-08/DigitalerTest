b02797de23ec05614109b3f2fd08f83281e3c3ea
Nicolas Beyer <nicolasj.beyer@outlook.de>
2020-06-19 10:01:13 +0200

    Timer

---
+++ DigitalerTest/Data/Stoppuhr.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DigitalerTest.Data
+{
+    public class Stoppuhr
+    {
+        public Timer timer;
+
+        public void starteStoppuhr()
+        {
+            timer = new Timer();
+
+            timer.Interval = 5000;
+
+            timer.Start();
+        }
+
+        public void stoppeStoppuhr()
+        {
+            timer.Stop();
+        }
+    }
+}
---
+++ DigitalerTest/Data/StoppuhrService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalerTest.Data
+{
+    public class StoppuhrService
+    {
+        public Task<Stoppuhr> starteStoppuhrAsync()
+        {
+            Stoppuhr stoppuhr = new Stoppuhr();
+
+            stoppuhr.starteStoppuhr();
+
+            return Task.FromResult(stoppuhr);
+        }
+    }
+}
