ba0b861a98e8ed8328c3ff4cb6312aa456630ecf
Nicolas Beyer <nicolasj.beyer@outlook.de>
2020-06-20 18:01:45 +0200

    Themengebiete sammeln + Loop für Durchlauf

--- DigitalerTest/Startup.cs
+++ DigitalerTest/Startup.cs
@@ -29,6 +29,7 @@ namespace DigitalerTest
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<StoppuhrService>();
+            services.AddSingleton<Eignungstest>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
