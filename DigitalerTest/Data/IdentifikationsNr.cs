88f9ae6f2fed90c32c845fb7a9f4acb1fbb8b3b8
Marcel Reinhardt <post4marcel@live.de>
2020-06-22 14:44:32 +0200

    Validierung + Abspeicherung

--- DigitalerTest/Data/IdentifikationsNr.cs
+++ DigitalerTest/Data/IdentifikationsNr.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using DigitalerTest;
 
 namespace DigitalerTest.Data
 {
     public class IdentifikationsNr
     {
         [Required]
-        [RegularExpression(^"[0-9]")]
+        [RegularExpression("[0-9]{7,7}", ErrorMessage = "Bitte überprüfen Sie ihre Identifikationsnummer!")]
         
-       
         public string IDNummer { get; set; }
+
+        public Expression<Func<IdentifikationsNr>> For { get; set; }
     }
 }
