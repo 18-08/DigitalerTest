0f6f9b13da7e38cb46bd24d0b26feb1367ec9c73
Nicolas Beyer <nicolasj.beyer@outlook.de>
2020-06-22 14:02:19 +0200

    Validierung

---
+++ DigitalerTest/Data/IdentifikationsNr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalerTest.Data
+{
+    public class IdentifikationsNr
+    {
+        [Required]
+        [RegularExpression(^"[0-9]")]
+        
+       
+        public string IDNummer { get; set; }
+    }
+}
